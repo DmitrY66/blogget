42045500525101b1cf32b1abac8553c1e72f8e38
Dmitri Gorun <iii448@mail.ru>
2022-08-09 02:17:05 +0500

    fix URL_HOME

--- src/api/auth.js
+++ src/api/auth.js
@@ -16,12 +16,3 @@ searchParams.append('redirect_uri', REDIRECT_URI);
 searchParams.append('scope', SCOPE);
 
 export const urlAuth = `${URL_AUTH}${searchParams.toString()}`;
-
-// console.log('urlAuth: ', urlAuth);
-
-
-// https://www.reddit.com/api/v1/authorize?client_id=CLIENT_ID&response_type=TYPE&state=RANDOM_STRING&redirect_uri=URI&scope=SCOPE_STRING
-
-// http://localhost:3000/auth#access_token=2056957797056-6yC6ZKIt0yk_hxtVwUXZmmcTbahqGQ&token_type=bearer&state=random_string&expires_in=86400&scope=read+submit+identity
-
-// http://localhost:3000/auth#access_token=2056957797056-6k3drWInbNgCEGy0-jcNZak8Ahb-LQ&token_type=bearer&state=random_string&expires_in=86400&scope=read+submit+identity
--- src/api/const.js
+++ src/api/const.js
@@ -1,4 +1,5 @@
-export const URL_HOME = 'http://localhost:3000';
+// export const URL_HOME = 'http://localhost:3000';
+export const URL_HOME = 'https://blogget-two.vercel.app';
 export const URL_API = 'https://oauth.reddit.com';
 export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'zMypstX4LkblmA-p9l9rqA';
